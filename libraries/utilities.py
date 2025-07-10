091be9f0363efea5268b7ee6905295f815b6ab17
ZywOo <48447010+ZywOo-S@users.noreply.github.com>
2025-07-10 09:46:39 +0800

    ZywOo

--- libraries/utilities.py
+++ libraries/utilities.py
@@ -35,10 +35,30 @@ def configure_platform():
             "MODEM_GPS_ENABLE_GPIO": 4,
             "MODEM_GPS_ENABLE_LEVEL": 1,
         }
+    elif CURRENT_PLATFORM == "LILYGO_T_A7670":
+        CONFIG = {
+            "MODEM_BAUDRATE": 115200,
+            "MODEM_DTR_PIN": 25,
+            "MODEM_TX_PIN": 26,
+            "MODEM_RX_PIN": 27,
+            "BOARD_PWRKEY_PIN": 4,
+            "BOARD_BAT_ADC_PIN": 35,
+            "BOARD_POWERON_PIN": 12,
+            "MODEM_RING_PIN": 33,
+            "MODEM_RESET_PIN": 5,
+            "BOARD_MISO_PIN": 2,
+            "BOARD_MOSI_PIN": 15,
+            "BOARD_SCK_PIN": 14,
+            "BOARD_SD_CS_PIN": 13,
+            "MODEM_RESET_LEVEL": 1,
+            "MODEM_GPS_ENABLE_GPIO": 0,
+            "MODEM_GPS_ENABLE_LEVEL": 0,
+        }
     else:
         raise ValueError("Unsupported platform")
 
     for key, value in CONFIG.items():
         globals()[key] = value
 
-set_platform("LILYGO_T_SIM7670G")
+# set_platform("LILYGO_T_SIM7670G")
+set_platform("LILYGO_T_A7670")
